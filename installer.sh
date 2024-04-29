c524c1a1b39ebfc9b3c0e039c243126802478e75
afimpel <afimpel@afimpel.com>
2024-04-29 01:30:42 -0300

    curl to wget

--- installer.sh
+++ installer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+URLgithub="https://raw.githubusercontent.com/afimpel/upgrade-system-debian"
 apt-get install sudo -y
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 echo "%wheel ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown, /usr/bin/upgrade-syste*" >> /etc/sudoers.d/wheel
 adduser $(getent passwd 1000 | cut -d: -f1) wheel
-curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-debian/main/upgrade-system > /usr/bin/upgrade-system
-curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-debian/main/upgrade-system-poweroff > /usr/bin/upgrade-system-poweroff
-curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-debian/main/upgrade-system-reboot > /usr/bin/upgrade-system-reboot
+wget --no-cache -O /usr/bin/upgrade-system ${URLgithub}/main/upgrade-system
+wget --no-cache -O /usr/bin/upgrade-system-poweroff ${URLgithub}/main/upgrade-system-poweroff
+wget --no-cache -O /usr/bin/upgrade-system-reboot ${URLgithub}/main/upgrade-system-reboot
 chmod 777 /usr/bin/upgrade-system* -R
