84fba059ef38f3689f4e693015b617537c985441
Alvaro Fimpel <afimpel@gmail.com>
2025-04-28 21:22:18 -0300

    style: :lipstick: xterm: install xdotool...

--- installer-desktop.sh
+++ installer-desktop.sh
@@ -3,7 +3,165 @@
 UUID=$(id -u)
 UUID=$(( UUID + 0 ))
 I_OK="✔"
-I_KO="✖️"
+I_KO="X"
+NC='\e[0m'
+
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[0;105m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
+
+R1 () {
+    local customColor0="$1"
+    local customText0="$2"
+    local customColor1="$3"
+    local customIcon0="$4"
+    local customRepeat0="$5"
+    shift 5
+    local ttyMode=$1
+    if [ "$ttyMode" = "tty" ]; then
+        printf " $customIcon0 $customText0 :\n"
+    else
+        data=$(completeLine "$customText0" "$customRepeat0" 1);
+        printf " ${customColor1}$customIcon0 ${NC}$customColor0 $data ${customColor1}$customIcon0${NC}\n"
+    fi
+}
+
+CUSTOM () {
+    local customColor0="$1"
+    local customText0="$2"
+    local customColor1="$3"
+    local customText1="$4"
+    local customColor2="$5"
+    local customIcon0="$6"
+    local customRepeat0="$7"
+    local customIcon1="$8"
+    local customNumber=$9
+    shift 9
+    local ttyMode=$1
+    if [ "$ttyMode" = "tty" ]; then
+        printf " $customIcon0 $customText0 : $customText1\n"
+    else
+        data=$(completeLine "$customText0" "$customRepeat0" 3 "$customText1" $customNumber);
+        printf " ${customColor2}${customIcon0} ${NC}${customColor0} $customText0${customColor1} $data ${customColor2}$customIcon1${NC}\n"
+    fi
+}
+
+L1 () {
+    local customColor0="$1"
+    local customText0="$2"
+    local customColor1="$3"
+    local customIcon0="$4"
+    local customRepeat0="$5"
+    shift 5
+    
+    local ttyMode=$1
+    if [ "$ttyMode" = "tty" ]; then
+        printf " $customIcon0 $customText0 :\n"
+    else
+        data=$(completeLine "$customText0" "$customRepeat0" 2);
+        printf " ${customColor1}$customIcon0 ${NC}$customColor0 $data ${customColor1}$customIcon0${NC}\n"
+    fi
+}
+
+completeLine() {
+    if [ "$3" == '3' ]; then
+        menos=$(( 11 + $5 ))
+    else
+        menos=10
+    fi
+    local input_string="$1$4"
+    local input_string0="$1"
+    local input_string1="$4"
+    local total_length=$(tput cols)-$menos
+    local input_length=${#input_string}
+    local num_dots=$((total_length - input_length))
+
+    if [ $num_dots -lt 0 ]; then
+        num_dots=1
+    fi
+
+    local output_string=""
+    for ((i=0; i<num_dots; i++)); do
+        output_string+="$2"
+    done
+    if [ "$3" == '3' ]; then
+        echo "$output_string $input_string1"
+    elif [ "$3" == '0' ]; then
+        echo "$input_string0 $output_string $input_string1"
+    elif [ "$3" == '1' ]; then
+        echo "$input_string $output_string"
+    else
+        echo "$output_string $input_string"
+    fi
+}
 
 if [ $UUID != 0 ]; then
     echo "${I_KO}    Start the Script as 'root' for it to work properly    ${I_KO}";
@@ -39,6 +197,7 @@ else
     chmod 777 -v /usr/share/applications/pkexec_*
     chmod 777 -v /usr/share/pixmaps/* -R
     chmod 777 -v /usr/bin/pkexec_upgrad* -R
+    sudo apt install xdotool -y
 
     echo " ";
 	dates=$(date +'%Y-%m-%d %H:%M')
